c1f9e2cabfe80ffe493051143a6412b485c9e30f
ellenaksv <ellen.anna.karin@gmail.com>
2021-10-08 11:09:14 +0200

    Comment with information
    Comment regarding coming additions to the class

--- Game/src/Game/Player.java
+++ Game/src/Game/Player.java
@@ -1,4 +1,10 @@
 package Game;
 
+/** name: String
+ * coins: int
+ * animalOwned: int (förmodligen en per sorts animal senare - lionOwned, dogOwned osv)
+ * foodOwned: int (förmodligen en per sorts mat senare - mixedOwned, meatOwned osv)
+ */
+
 public class Player {
 }
