375c320e4e45236974237cf6f3b48d510d4e5087
ellenaksv <ellen.anna.karin@gmail.com>
2021-10-08 13:30:55 +0200

    Updated Store variables

--- Game/src/Game/Store.java
+++ Game/src/Game/Store.java
@@ -10,6 +10,27 @@ package Game;
  */
 
 public class Store {
-    private int animalPrice;
-    private int foodPrice;
+    public final int lionPrice = 100;
+    public final int bearPrice = 80;
+    public final int horsePrice = 50;
+    public final int dogPrice = 30;
+    public final int rabbitPrice = 10;
+
+    public final int meatPrice = 10;
+    public final int mixedPrice = 5;
+    public final int vegPrice = 1;
+
+
+    public Store (int lionPrice, int bearPrice, int horsePrice, int dogPrice, int rabbitPrice, int meatPrice, int mixedPrice, int vegPrice) {
+        this.lionPrice = lionPrice;
+        this.bearPrice = bearPrice;
+        this.horsePrice = horsePrice;
+        this.dogPrice = dogPrice;
+        this.rabbitPrice = rabbitPrice;
+
+        this.meatPrice = meatPrice;
+        this.mixedPrice = mixedPrice;
+        this.vegPrice = vegPrice;
+        
+    }
 }
