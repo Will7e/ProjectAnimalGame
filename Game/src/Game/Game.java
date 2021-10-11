04782d559087456a670443c0fb12e229791397b3
MarcusBrederfalt <marcus.brederfalt@gmail.com>
2021-10-11 14:18:41 +0200

    Update game.java
    Added two int variables gameRounds & numberOfPlayers.

--- Game/src/Game/Game.java
+++ Game/src/Game/Game.java
@@ -1,15 +1,23 @@
 package Game;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class Game {
 
     private List<Player> playersList;
+    private int gameRounds;
+    private int numberOfPlayers;
 
     public Game() {
         this.playersList = new ArrayList<>();
 
     }
 
-    public void gameRound() {
+    public void game() {
+
+
 
     }
 
@@ -19,9 +27,21 @@ public class Game {
 
     public void addPlayer(Player player) {
 
-        
     }
 
+    public int getGameRounds() {
+        return gameRounds;
+    }
 
+    public void setGameRounds(int gameRounds) {
+        this.gameRounds = gameRounds;
+    }
 
+    public int getNumberOfPlayers() {
+        return numberOfPlayers;
+    }
+
+    public void setNumberOfPlayers(int numberOfPlayers) {
+        this.numberOfPlayers = numberOfPlayers;
+    }
 }
