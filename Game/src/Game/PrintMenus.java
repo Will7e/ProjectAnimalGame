897ae18082f1098d2e567c29c7353604a692eb59
ellenaksv <ellen.anna.karin@gmail.com>
2021-10-11 13:59:59 +0200

    Added Scanner to startMenu

--- Game/src/Game/PrintMenus.java
+++ Game/src/Game/PrintMenus.java
@@ -1,5 +1,7 @@
 package Game;
 
+import java.util.Scanner;
+
 public class PrintMenus {
 
     //Skapa metoder för att skriva ut menyer till spelet
@@ -8,5 +10,9 @@ public class PrintMenus {
         System.out.println("This is a game where you buy and sell animals.");
         System.out.println("Chose an option to start a new game:");
         System.out.println("1. Start a new game 2. Start from a saved file");
+
+        Scanner myScanner = new Scanner(System.in);
+        String startMenuChoice = myScanner.nextLine();
+        
     }
 }
