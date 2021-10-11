9d5f77384e68b253eea184720db26c6febefae86
ellenaksv <ellen.anna.karin@gmail.com>
2021-10-11 14:32:38 +0200

    Changed phrasing

--- Game/src/Game/PrintMenus.java
+++ Game/src/Game/PrintMenus.java
@@ -5,7 +5,7 @@ public class PrintMenus {
 
     //Skapa metoder för att skriva ut menyer till spelet
     public static void startMenu(){
-        System.out.println("Hi and welcome to the Animal Game!");
+        System.out.println("Welcome to the Animal Game!");
         System.out.println("This is a game where you buy and sell animals.");
         System.out.println("Chose an option to start a new game:");
         System.out.println("1. Start a new game 2. Start from a saved file");
