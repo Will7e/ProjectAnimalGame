b226fc6364b0d6dff57c369e771dae03f8243958
ellenaksv <ellen.anna.karin@gmail.com>
2021-10-11 13:54:42 +0200

    Start menu - printed statements

--- Game/src/Game/PrintMenus.java
+++ Game/src/Game/PrintMenus.java
@@ -3,4 +3,10 @@ package Game;
 public class PrintMenus {
 
     //Skapa metoder för att skriva ut menyer till spelet
+    public static void startMenu {
+        System.out.println("Hi and welcome to the Animal Game!");
+        System.out.println("This is a game where you buy and sell animals.");
+        System.out.println("Chose an option to start a new game:");
+        System.out.println("1. Start a new game 2. Start from a saved file");
+    }
 }
